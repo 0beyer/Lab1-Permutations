5df31b8b56ef14eb8f879a9fa23bf088012e1991
Owen Beyer <156833692+0beyer@users.noreply.github.com>
2025-08-25 14:49:42 -0500

    Added Lab
    Added lab that i did in a local file, now its a repo so turning it in is easier

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Main { 
+    // Owen and Haider's lab
+    public static void main(String[] args) {
+
+        // takes an integer, and creates a string of the natural numbers from 1 to n
+        // to run, pick your size, set n to that
+        int n = 3;
+        ArrayList<Integer> Arr = new ArrayList<Integer>();
+        for (int i = 1; i < n + 1; i++) {
+            Arr.add(i);
+        } 
+        permuteTime(0, Arr);
+    }
+
+    // swap the ith and jth index
+    static void swap(ArrayList<Integer> Arr, int i, int j) {
+        int temp = Arr.get(i);
+        Arr.set(i, Arr.get(j));
+        Arr.set(j, temp);
+    }
+
+    // this is a recursive function that will give us the permutations
+    static void recurPermute(int index, ArrayList<Integer> Arr) {
+
+        if (Arr.size() == 0) {
+            System.err.println("String is empty ;(");
+        }
+        
+        // the base case, when the length of the string is the same as the index,
+        // it prints the string
+        if (index == Arr.size()) {
+            System.out.println(Arr);
+        }
+
+        // swaps current index with all possible indices, then recur
+        for (int i = index; i < Arr.size(); i++) {
+
+            swap(Arr, i, index);
+            recurPermute(index + 1, Arr);
+            swap(Arr, i, index);
+        }
+    }
+
+    // function that calls
+    static void permuteTime(int index, ArrayList<Integer> Arr) {
+        long t1 = System.currentTimeMillis();
+        recurPermute(0, Arr);
+        long t2 = System.currentTimeMillis();
+        System.out.println(t2 - t1);
+    }
+}
---
+++ Questions.md
@@ -0,0 +1,12 @@
+Lab Questions:
+    1: What is the number of lines of output as a function of n? Explain how you computed it.
+
+    n!, because n! is the amount of permutations that a list of numbers n large has
+
+    2: Based on your timing results and on approximation of the number of lines printed please estimate how long your program would run for n = 15 and for n = 20. Show how you have computed the estimate.
+
+    because n = 8 took 1100 is ms, and n = 9 it took 8700ish ms, we can create a ratio with 
+    8450/9!. to get an estimate of n, do (n!*.02329749)
+    ratio for our function
+    for n = 15, it would take 3.135132e10 ms
+    for n = 20, it would take 5.83285038336e16 ms
